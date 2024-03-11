db16abef788904c275b668591f0566d04b86c7de
ForkahEH <elmaforkah@gmail.com>
2024-03-11 18:54:59 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -48,6 +48,13 @@ class TestAccountService(TestCase):
         db.session.commit()
 
         self.client = app.test_client()
+    
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
 
     def tearDown(self):
         """Runs once after each test case"""
